c194ac175773b7738d951ffdf3f337b57b099154
jfallmann <fall@bioinf.uni-leipzig.de>
2018-12-13 15:47:23 +0100

    Revised version of scripts, thx to Xavier for pointing this out

--- removeGenomeMapper.pl
+++ removeGenomeMapper.pl
@@ -1,12 +1,15 @@
 #!/usr/bin/env perl
 
 use strict;
+use PerlIO::gzip;
 
 my $file1 = shift; #pre-tRNAs
 my $file2 = shift; #sam
 my $out   = shift; #filtered sam
-open FILE1, "< $file1" or die "can t open $file1\n";
-open FILE2, "< $file2" or die "can t open $file2\n";
+my $flank = shift || 50;
+
+open FILE1, "<:gzip(autopop)", "$file1" or die "can t open $file1\n";
+open FILE2, "<:gzip(autopop)", "$file2" or die "can t open $file2\n";
 open OUT,   "> $out" or die "can t open $out\n";
 
 my %ID   = ();
@@ -16,27 +19,37 @@ my @SAM  = ();
 
 while(<FILE1>){
         chomp $_;
-	
+
 	if($_=~ m/>/){
 		my @id = split/>/,$_;
 		$ID{$id[1]} = 0;
 	}
 }
 
-
 while(<FILE2>){
-        chomp $_;
-	if($_=~ m/@/){
-		print OUT $_."\n";
-	}
-
+	chomp $_;
+	
 	my @LINE = split/\t/,$_;
 
-	if(!exists $SEQ{$LINE[0]}){
-		$SEQ{$LINE[0]} = 0;
+	if($LINE[0] =~ m/^@/){
+		print OUT $_."\n";
+		next;
 	}
-	if(!exists $ID{$LINE[2]}){
-		$SEQ{$LINE[0]} = 1;
+	
+	if(length $LINE[9] >= $flank || length $LINE[9] >= 30){ # Filtering for read length longer than flank or 30
+		
+		if(!exists $SEQ{$LINE[0]}){
+			$SEQ{$LINE[0]} = 0;
+		}
+		
+		if(!exists $ID{$LINE[2]}){
+			$SEQ{$LINE[0]} = 1;
+		}
+	}
+	else{
+		if(!exists $SEQ{$LINE[0]}){
+			$SEQ{$LINE[0]} = 0;
+		}
 	}
 	push @SAM,$_;
 }
@@ -45,8 +58,6 @@ while(<FILE2>){
 foreach (@SAM){
 	my @LINE = split/\t/,$_;
 	if($SEQ{$LINE[0]} eq 0){
-		print OUT $_."\n"; 
+		print OUT $_."\n";
 	}
 }
-
-
--- removePrecursor.pl
+++ removePrecursor.pl
@@ -1,60 +1,58 @@
 #!/usr/bin/perl
 use strict;
+use PerlIO::gzip;
 
 my $file1 = shift; #pre-tRNAs bed12
 my $file2 = shift; #sam
 my $file3 = shift; #fq
+my $flank = shift || 50; #length of added flanking region
 
-open FILE1, "< $file1" or die "can t open $file1\n";
-open FILE2, "< $file2" or die "can t open $file2\n";
+open FILE1, "<:gzip(autopop)", "$file1" or die "can t open $file1\n";
+open FILE2, "<:gzip(autopop)", "$file2" or die "can t open $file2\n";
 open(FILE3, "gunzip -c $file3|") or die "Cannot open $file3\n";
 
 my %tRNA  = ();
 my %read  = ();
 my @entry = ();
 
+my $flankEnd = $flank - 6; # consider CCACCA tail 
 
 while(<FILE1>){
-        chomp $_;
-		
+	chomp $_;	
 	my @id = split/\t/,$_;
 	$tRNA{$id[3]} = $id[10];  #length
 }
 
-
 while(<FILE2>){
-        chomp $_;
+	chomp $_;
 
 	my @id = split/\t/,$_;
-
 	my $start  = $id[3];
 	my $len    = length($id[9]); 
 	my @cigar = split(/(I|M|X|D)/, $id[5]);
-		my $I = 0;
-		my $D = 0;
-		for(my $i=0; $i < scalar(@cigar); $i++){
-       			if($cigar[$i] eq "I"){ $I += $cigar[$i-1];}
-			if($cigar[$i] eq "D"){ $D += $cigar[$i-1];}
-		}
+	my $I = 0;
+	my $D = 0;
+	for(my $i=0; $i < scalar(@cigar); $i++){
+		if($cigar[$i] eq "I"){ $I += $cigar[$i-1];}
+		if($cigar[$i] eq "D"){ $D += $cigar[$i-1];}
+	}
 	my $end    = $start + $len -1 + $D - $I;
 
-	if($start > 50 && $end <= $tRNA{$id[2]}-46){
+	if($start > $flank && $end <= $tRNA{$id[2]}-$flankEnd){
 		$read{$id[0]} = 0;
 	}	
 }
 
-
 while(<FILE3>){
-        chomp;
-        push @entry, $_;
+	chomp;
+	push @entry, $_;
 
-        if (scalar(@entry) == 4) {
-                my ($id, $seq, $plusLine, $qual) = @entry;
-                @entry = ();
-                my @ID = split/\@/,$id;
-                if(exists $read{$ID[1]}){
-                        print "$id\n$seq\n$plusLine\n$qual\n";
-                }
-        }
+	if (scalar(@entry) == 4) {
+		my ($id, $seq, $plusLine, $qual) = @entry;
+		@entry = ();
+		my @ID = split/\@/,$id;
+		if(exists $read{$ID[1]}){
+			print "$id\n$seq\n$plusLine\n$qual\n";
+		}
+	}
 }
-
